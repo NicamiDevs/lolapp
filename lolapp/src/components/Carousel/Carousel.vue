0632826093fae91673860c44e4829beb7b1e5984
Nicolas <nicopua7@gmail.com>
2023-07-04 02:21:48 -0300

    NavBar, Carousel, Home

---
+++ lolapp/src/components/Carousel/Carousel.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="my-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://cdnportal.mobalytics.gg/production/2018/08/Stats-tab-splash.png" class="d-block w-100" alt="Slide 1">
+            </div>
+            <div class="carousel-item">
+                <img src="https://www.leagueoflegends.com/static/open-graph-2e582ae9fae8b0b396ca46ff21fd47a8.jpg" class="d-block w-100" alt="Slide 2">
+            </div>
+            <div class="carousel-item">
+                <img src="https://cdnportal.mobalytics.gg/production/2018/08/Stats-tab-splash.png" class="d-block w-100" alt="Slide 3">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Esto es un texto</h5>
+                    <p>Texto mucho más chikito</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#my-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#my-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</template>
--- lolapp/src/components/NavBar/NavBar.vue
+++ lolapp/src/components/NavBar/NavBar.vue
@@ -1,6 +1,15 @@
 <template>
-    <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" style="height: 3rem;">
+        <div class="collapse navbar-collapse d-flex justify-content-start" style="width: 500px;">
+            <div class="d-flex" style="width: 250px;">
+                <div class="navbar-brand text-light">NicamiLoL</div>
+                <RouterLink to="/" class="navbar-brand text-light">Home</RouterLink>
+                <RouterLink to="/about" class="navbar-brand text-light">About Us</RouterLink>
+            </div>
+        </div>
+        <form class="form-inline d-flex mr-5" v-if="$route.path === '/'">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
     </nav>
 </template>
